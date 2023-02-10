a38e7bca3e2dac3afd35ee1b09c582627a0bd81d
amitvish123 <94536584+amitvish123@users.noreply.github.com>
2023-02-10 18:18:22 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector('.changeColorBtn');
 
 btn.addEventListener('click',()=>{
-      document.body.appendChild("userForm.html")
+      document.open("userForm.html");
     });
